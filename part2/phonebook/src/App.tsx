a17d878a40386ca481569475c2f1e3259384b393
rifthoss <rifthoss@amazon.com>
2024-03-14 16:08:01 -0500

    initialize vite projects for part3-7

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,43 +1,26 @@
 import { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setNewName(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    setPersons((prevPersons) => [...prevPersons, { name: newName }]);
-    setNewName("");
-  };
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
 
   return (
     <div>
-      <div>debug: {newName}</div>
-      <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input onChange={handleChange} value={newName} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <h2>Phone book</h2>
+      <Filter persons={persons} />
+      <h2>Add a new</h2>
+      <PersonForm persons={persons} setPersons={setPersons} />
+
       <h2>Numbers</h2>
-      <div>
-        {persons.map((person) => {
-          return (
-            <div key={person.name}>
-              {person.name}
-              <br />
-            </div>
-          );
-        })}
-      </div>
-      ...
+
+      <Persons persons={persons} />
     </div>
   );
 }
